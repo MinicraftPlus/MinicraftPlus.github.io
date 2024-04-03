e7a9a34e6cb912b283a83256e5855a0686e49aff
Makkkkus <37084190+Makkkkus@users.noreply.github.com>
2024-04-03 19:42:59 +0200

    Remove Jekyll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Website for Minicraft+</h1>
-<p>New page is under construction. Meanwhile take a look at <a data-sveltekit-reload rel="external" href="./old">our old site</a>.</p>
+<p>New page is under construction. Meanwhile take a look at <a data-sveltekit-reload rel="external" href="./old/index.html">our old site</a>.</p>

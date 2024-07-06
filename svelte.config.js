d298b68f7cb1bb2e40f7d8edb3f235a4e7b345b8
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2024-07-06 14:26:25 +0800

    Try to fix build

--- svelte.config.js
+++ svelte.config.js
@@ -5,10 +5,10 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html',
+			// fallback: '404.html',
 			pages: './build',
 			assets: './build',
-			strict: true
+			// strict: true
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH

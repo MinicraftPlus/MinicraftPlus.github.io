7c5cb6b3d847f9382b171accc66f42bfe45aa64f
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2024-07-06 13:51:21 +0800

    Try to fix build

--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,6 @@ const config = {
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		},
-		prerender: {
-			default: true
-		}
 	},
 	preprocess: vitePreprocess()
 };

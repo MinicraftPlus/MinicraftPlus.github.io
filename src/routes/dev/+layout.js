58314145862153e188f2581036046614923add10
BenCheung0422 <74168521+BenCheung0422@users.noreply.github.com>
2024-07-06 13:29:10 +0800

    Try to fix build

--- src/routes/dev/+layout.js
+++ src/routes/dev/+layout.js
@@ -6,10 +6,16 @@ import {dev} from "$app/environment";
 // Source: https://stackoverflow.com/a/73638129
 // Source: https://stackoverflow.com/a/68074574
 
+export function load() {
+	if (!dev) {
+		error(404, "Not Found");
+	}
+}
+
 // if (!dev) {
 // 	error(404, "Not Found");
 // }
 
-export const prerender = dev;
+// export const prerender = dev;
 export const ssr = dev;
 export const csr = dev;

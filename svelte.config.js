052f95a360528248d70c0bd79c3cee5ea0fe1b5a
BatchDebug <dieeinzigenkisten@gmail.com>
2025-02-19 08:51:54 +0100

    Attempt 2 to fix stuff

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html',
+			pages: './build',
+			assets: './build'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		},
+	},
+	preprocess: vitePreprocess()
+};
+
+export default config;
